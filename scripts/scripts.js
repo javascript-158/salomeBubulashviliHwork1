cbe2c4aec5ff7249baf803da1022b3c6e86d0356
salomebubulashvili <Bubulashviliii@gmail.com>
2023-11-22 12:06:42 +0400

    add js

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+let firstName = prompt ("ჩაწერეთ თქვენი სახელი") ;
+
+let lastName = propmt ("ჩაწერეთ თქვენი გვარი") ;
+
+let birthYear = prompt ("ჩაწერეთ თქვენი დაბადების წელი") ;
+
+let yourAddress = prompt ("ჩაწერეთ თქვენი საცხოვრებელი ადგილი") ;
+
+const presentYear = 2023;
+
+let userAge = currentYear - birthYear ;
+
+alert ("გამარჯობა, " + firstName + " " + lastName + " " + "! მიხარია, რომ უკვე" + userAge + "ხარ და" + yourAddress + "-ში ცხოვრობ" );
